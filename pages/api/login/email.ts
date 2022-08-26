1bc24121aa40a03646805cbf7244f63d46d88af7
tohsaka888 <156132264@qq.com>
2022-08-26 14:05:06 +0800

    ✨ feat(api): 邮箱登录

---
+++ pages/api/login/email.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import Cors from 'cors'
+import { runMiddleware } from '@utils/server/runMiddleware'
+import { connectDB } from '@utils/server/connectDB'
+
+
+
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  try {
+
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+    const db = await connectDB()
+    if (db) {
+      // select collection
+      const users = await db.collection('users')
+
+      const body: { email: string; password: string } = req.body
+      const result = await users.findOne({ ...body })
+
+      res.status(200).json({ success: true, canLogin: result ? true : false })
+    } else {
+      new Error('数据库连接失败')
+    }
+  } catch (error: any) {
+    res.status(200).send({
+      success: false,
+      error: (error as Error).message
+    })
+  }
+}
