838aa1ec24c6d2cc71109048730ccaafe8f16b9b
tohsaka888 <156132264@qq.com>
2022-08-29 14:16:50 +0800

    ✨ feat: 使用国内数据库

--- utils/server/connectDB.ts
+++ utils/server/connectDB.ts
@@ -1,6 +1,6 @@
 import { Db, MongoClient } from 'mongodb'
 
-const uri = process.env.mongoUri as string
+const uri = process.env.MONGODB_URI as string
 
 const client = new MongoClient(uri)
 
@@ -11,7 +11,7 @@ export const connectDB = async () => {
     try {
       await client.connect()
       db = await client.db('cxy-home')
-    } catch {
+    } finally {
       await client.close()
     }
   }
